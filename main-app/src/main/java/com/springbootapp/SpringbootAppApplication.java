9f3f3e7427f32aa757b00688cd0ffbe143ff1ad4
damianmore <dmore0126@gmail.com>
2025-10-19 15:06:58 -0700

    Moved springboot application files into a new main-app folder. This is because I will be setting up a microservice architecture and want the components to be completely seperate.

--- main-app/src/main/java/com/springbootapp/SpringbootAppApplication.java
+++ main-app/src/main/java/com/springbootapp/SpringbootAppApplication.java
@@ -2,12 +2,20 @@ package com.springbootapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootAppApplication.class, args);
+    }
+
+    @GetMapping
+    public String index() {
+        return "Hello World";
     }
 
 }
